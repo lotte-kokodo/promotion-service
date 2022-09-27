d48910b3f7b121a819ccaf30a6d34252c7f00ca0
SSOsh <tngh147258@naver.com>
2022-09-27 19:15:28 +0900

    Auto stash before merge of "feat/rate-discount-policy" and "origin/dev"

--- src/main/java/shop/kokodo/promotionservice/controller/RateDiscountPolicyController.java
+++ src/main/java/shop/kokodo/promotionservice/controller/RateDiscountPolicyController.java
@@ -1,8 +1,14 @@
 package shop.kokodo.promotionservice.controller;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import shop.kokodo.promotionservice.dto.FixDiscountPolicyDto;
+import shop.kokodo.promotionservice.dto.RateDiscountPolicyDto;
+import shop.kokodo.promotionservice.entity.FixDiscountPolicy;
+import shop.kokodo.promotionservice.entity.RateDiscountPolicy;
 import shop.kokodo.promotionservice.service.RateDiscountPolicyService;
 
 @RestController
@@ -12,6 +18,24 @@ public class RateDiscountPolicyController {
     @Autowired
     public RateDiscountPolicyController(RateDiscountPolicyService rateDiscountPolicyService) {
         this.rateDiscountPolicyService = rateDiscountPolicyService;
+    }
+
+    @PostMapping("/save")
+    public FixDiscountPolicy save(@RequestBody FixDiscountPolicyDto fixDiscountPolicyDto){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        FixDiscountPolicy fixDiscountPolicy = mapper.map(fixDiscountPolicyDto, FixDiscountPolicy.class);
+        return fixDiscountPolicy;
+    }
+
+    @GetMapping(value="/ratediscount/{productId}")
+    public RateDiscountPolicy getRateDiscountPolicy(@PathVariable("productId")Long productId) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        RateDiscountPolicyDto rateDiscountPolicyDto = rateDiscountPolicyService.getRateDiscountPolicy(productId);
+        RateDiscountPolicy rateDiscountPolicy = mapper.map(rateDiscountPolicyDto, RateDiscountPolicy.class);
+        return rateDiscountPolicy;
     }
 }
 
