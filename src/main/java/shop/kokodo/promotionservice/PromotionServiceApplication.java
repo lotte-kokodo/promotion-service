5774d8c7e73a932d5ef198b854db8e3f069e4bcc
SSOsh <tngh147258@naver.com>
2022-09-28 17:15:59 +0900

    Feat : 등록일자, 수정일자 등록 오류 수정

--- src/main/java/shop/kokodo/promotionservice/PromotionServiceApplication.java
+++ src/main/java/shop/kokodo/promotionservice/PromotionServiceApplication.java
@@ -2,8 +2,10 @@ package shop.kokodo.promotionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PromotionServiceApplication {
 
 	public static void main(String[] args) {
