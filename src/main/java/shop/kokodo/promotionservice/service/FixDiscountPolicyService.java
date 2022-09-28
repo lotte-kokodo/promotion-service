41e81d25135ee0326f4c47de61a79aa151baa50c
SSOsh <tngh147258@naver.com>
2022-09-28 17:14:28 +0900

    Feat : 고정할인정책 등록, 전체조회, 상품ID조회 API 기능 추가

--- src/main/java/shop/kokodo/promotionservice/service/FixDiscountPolicyService.java
+++ src/main/java/shop/kokodo/promotionservice/service/FixDiscountPolicyService.java
@@ -1,4 +1,12 @@
 package shop.kokodo.promotionservice.service;
 
+import shop.kokodo.promotionservice.dto.FixDiscountPolicyDto;
+import shop.kokodo.promotionservice.entity.FixDiscountPolicy;
+
+import java.util.List;
+
 public interface FixDiscountPolicyService {
+    public FixDiscountPolicy save(FixDiscountPolicyDto fixDiscountPolicyDto);
+    public List<FixDiscountPolicy> getAll();
+    public FixDiscountPolicy getFixDiscountPolicy(Long productId);
 }
--- src/main/java/shop/kokodo/promotionservice/service/FixDiscountPolicyServiceImpl.java
+++ src/main/java/shop/kokodo/promotionservice/service/FixDiscountPolicyServiceImpl.java
@@ -1,15 +1,46 @@
 package shop.kokodo.promotionservice.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import shop.kokodo.promotionservice.dto.FixDiscountPolicyDto;
+import shop.kokodo.promotionservice.entity.FixDiscountPolicy;
+import shop.kokodo.promotionservice.entity.RateDiscountPolicy;
 import shop.kokodo.promotionservice.repository.FixDiscountPolicyRepository;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Service
+@Slf4j
 public class FixDiscountPolicyServiceImpl implements FixDiscountPolicyService {
     private FixDiscountPolicyRepository fixDiscountPolicyRepository;
 
     @Autowired
     public FixDiscountPolicyServiceImpl(FixDiscountPolicyRepository fixDiscountPolicyRepository) {
         this.fixDiscountPolicyRepository = fixDiscountPolicyRepository;
+    }
+
+    @Override
+    @Transactional
+    public FixDiscountPolicy save(FixDiscountPolicyDto fixDiscountPolicyDto) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        FixDiscountPolicy fixDiscountPolicy = mapper.map(fixDiscountPolicyDto, FixDiscountPolicy.class);
+        return fixDiscountPolicyRepository.save(fixDiscountPolicy);
+    }
+
+    @Override
+    @Transactional
+    public List<FixDiscountPolicy> getAll() {
+        return fixDiscountPolicyRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public FixDiscountPolicy getFixDiscountPolicy(Long productId) {
+        return fixDiscountPolicyRepository.findByProductId(productId).orElse(new FixDiscountPolicy());
     }
 }
