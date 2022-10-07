66bcb50cda6bd7ea7d503660f7a3a2878128560b
ohjiwon-201733026 <ojw970725@gmail.com>
2022-10-07 18:01:43 +0900

    Feat: CORS 설정 추가

---
+++ src/main/java/shop/kokodo/promotionservice/config/WebConfig.java
@@ -0,0 +1,21 @@
+package shop.kokodo.promotionservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("GET", "POST", "DELETE", "PUT")
+                .allowedOrigins("http://localhost:9090")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
