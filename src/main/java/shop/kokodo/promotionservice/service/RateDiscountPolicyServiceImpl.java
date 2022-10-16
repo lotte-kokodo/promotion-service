5be0f700bb080b62fb13064b4778dfc2101c267d
SSOsh <tngh147258@naver.com>
2022-10-16 21:47:49 +0900

    Feat : 비율할인정책 예외처리 기능 추가

--- src/main/java/shop/kokodo/promotionservice/service/RateDiscountPolicyServiceImpl.java
+++ src/main/java/shop/kokodo/promotionservice/service/RateDiscountPolicyServiceImpl.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -65,6 +66,15 @@ public class RateDiscountPolicyServiceImpl implements RateDiscountPolicyService 
 
     @Transactional
     public Response findByProductId(Long productId) {
-        return Response.success(rateDiscountPolicyRepository.findByProductId(productId).get());
+        Optional<RateDiscountPolicy> rateDiscountPolicy = rateDiscountPolicyRepository.findByProductId(productId);
+        RateDiscountPolicy result;
+        try {
+            result = rateDiscountPolicy.orElseThrow();
+        }catch(Exception e) {
+            result = new RateDiscountPolicy();
+            result.setRate(0);
+        }
+
+        return Response.success(result);
     }
 }
